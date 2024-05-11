ed71cc422a8cb8ee1ad347ef721f475fd1139558
Mahayoga <myoga.bahtiar@gmail.com>
2024-05-11 19:29:11 +0700

    Backup Database Terbaru

--- db_tokoputranew.sql
+++ db_tokoputranew.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 06 Bulan Mei 2024 pada 04.53
+-- Waktu pembuatan: 11 Bulan Mei 2024 pada 14.28
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.0.30
 
@@ -53,34 +53,22 @@ INSERT INTO `data_pengguna` (`id_pengguna`, `username`, `password`, `role`) VALU
 
 CREATE TABLE `detail_barang` (
   `kode_barang` char(20) NOT NULL,
-  `tgl_kadaluarsa` date NOT NULL
+  `tgl_kadaluarsa` date NOT NULL,
+  `status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data untuk tabel `detail_barang`
 --
 
-INSERT INTO `detail_barang` (`kode_barang`, `tgl_kadaluarsa`) VALUES
-('BR0006', '2025-05-14'),
-('BR0006', '2025-05-09'),
-('BR0007', '2025-06-02'),
-('BR0007', '2025-05-17'),
-('BR0005', '2025-03-18'),
-('BR0005', '2024-03-11'),
-('BR0003', '2024-05-16'),
-('BR0004', '2024-09-12'),
-('BR0001', '2024-10-16'),
-('BR0001', '2024-09-11'),
-('BR0012', '2024-09-12'),
-('BR0008', '2025-05-01'),
-('BR0010', '2024-08-15'),
-('BR0009', '2024-08-30'),
-('BR0009', '2024-08-15'),
-('BR0010', '2024-08-07'),
-('BR0011', '2024-10-14'),
-('BR0011', '2024-09-08'),
-('BR0011', '2024-09-18'),
-('BR0011', '2024-10-09');
+INSERT INTO `detail_barang` (`kode_barang`, `tgl_kadaluarsa`, `status`) VALUES
+('MN0006', '2024-05-10', 'Belum Terbuang'),
+('MN0006', '2024-05-12', 'Belum Terbuang'),
+('MN0006', '2024-05-10', 'Belum Terbuang'),
+('KP0001', '2024-05-12', 'Belum Terbuang'),
+('KP0001', '2024-05-12', 'Belum Terbuang'),
+('KP0002', '2024-05-14', 'Belum Terbuang'),
+('KP0002', '2024-05-14', 'Belum Terbuang');
 
 -- --------------------------------------------------------
 
@@ -95,24 +83,6 @@ CREATE TABLE `detail_transaksi` (
   `jumlah_barang` int(11) NOT NULL,
   `harga_barang` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Dumping data untuk tabel `detail_transaksi`
---
-
-INSERT INTO `detail_transaksi` (`id_transaksi`, `kode_barang`, `tanggal`, `jumlah_barang`, `harga_barang`) VALUES
-('TR0001', 'BR0002', '2024-05-06', 4, '12000'),
-('TR0001', 'BR0007', '2024-05-06', 2, '7000'),
-('TR0001', 'BR0012', '2024-05-06', 1, '1000'),
-('TR0002', 'BR0006', '2024-05-06', 6, '30000'),
-('TR0002', 'BR0009', '2024-05-06', 1, '2000'),
-('TR0003', 'BR0003', '2024-05-07', 1, '45000'),
-('TR0004', 'BR0003', '2024-05-08', 1, '45000'),
-('TR0004', 'BR0006', '2024-05-08', 1, '5000'),
-('TR0005', 'BR0008', '2024-05-08', 2, '6000'),
-('TR0005', 'BR0010', '2024-05-08', 3, '6000'),
-('TR0005', 'BR0011', '2024-05-08', 2, '2000'),
-('TR0005', 'BR0006', '2024-05-08', 2, '10000');
 
 -- --------------------------------------------------------
 
@@ -144,6 +114,7 @@ INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
 --
 
 CREATE TABLE `stok_barang` (
+  `id_barang` char(20) NOT NULL,
   `kode_barang` char(20) NOT NULL,
   `id_supplier` int(11) NOT NULL,
   `nama_barang` varchar(70) NOT NULL,
@@ -157,19 +128,19 @@ CREATE TABLE `stok_barang` (
 -- Dumping data untuk tabel `stok_barang`
 --
 
-INSERT INTO `stok_barang` (`kode_barang`, `id_supplier`, `nama_barang`, `stok_tersedia`, `harga_beli`, `harga_jual`, `kategori_barang`) VALUES
-('BR0001', 1, 'Indomie Goreng', 100, 2500, 3000, 3),
-('BR0002', 1, 'Mie Sedap', 100, 2500, 3000, 3),
-('BR0003', 2, 'Beras Kepompong 3KG', 10, 40000, 45000, 3),
-('BR0004', 2, 'Beras Kepompong 5KG', 15, 65000, 70000, 3),
-('BR0005', 2, 'Beras Apel Merah 5KG', 7, 70000, 72000, 3),
-('BR0006', 3, 'Aqua Botol 1.5L', 30, 4500, 5000, 2),
-('BR0007', 3, 'Aqua Botol 600ml', 14, 2500, 3500, 2),
-('BR0008', 3, 'Teh Pucuk 350ml', 31, 2500, 3000, 2),
-('BR0009', 4, 'Wafer Nabati Keju 39g', 11, 1500, 2000, 1),
-('BR0010', 4, 'Wafer Nabati Coklat 39g', 23, 1500, 2000, 1),
-('BR0011', 4, 'Wafer SIIP', 26, 500, 1000, 1),
-('BR0012', 5, 'Promag', 29, 500, 1000, 5);
+INSERT INTO `stok_barang` (`id_barang`, `kode_barang`, `id_supplier`, `nama_barang`, `stok_tersedia`, `harga_beli`, `harga_jual`, `kategori_barang`) VALUES
+('KP0001', '', 1, 'Indomie Goreng', 100, 2500, 3000, 3),
+('KP0002', '', 1, 'Mie Sedap', 100, 2500, 3000, 3),
+('KP0003', '', 2, 'Beras Kepompong 3KG', 10, 40000, 45000, 3),
+('KP0004', '', 2, 'Beras Kepompong 5KG', 15, 65000, 70000, 3),
+('KP0005', '', 2, 'Beras Apel Merah 5KG', 7, 70000, 72000, 3),
+('MK0009', '', 4, 'Wafer Nabati Keju 39g', 11, 1500, 2000, 1),
+('MK0010', '', 4, 'Wafer Nabati Coklat 39g', 23, 1500, 2000, 1),
+('MK0011', '', 4, 'Wafer SIIP', 26, 500, 1000, 1),
+('MN0006', '', 3, 'Aqua Botol 1.5L', 30, 4500, 5000, 2),
+('MN0007', '', 3, 'Aqua Botol 600ml', 14, 2500, 3500, 2),
+('MN0008', '', 3, 'Teh Pucuk 350ml', 31, 2500, 3000, 2),
+('OB0012', '909090909', 5, 'Promag Tablet', 29, 500, 1000, 5);
 
 -- --------------------------------------------------------
 
@@ -195,7 +166,8 @@ INSERT INTO `suppliers` (`id_supplier`, `nama_supplier`, `no_telp`, `alamat_supp
 (2, 'Rangga', '082336374821', 'Gersik', 'ranggak@gmail.com', 'Perusahaan DEF'),
 (3, 'Adam', '082336374821', 'Jember', 'adam@gmail.com', 'Perusahaan AH'),
 (4, 'Irsyad', '082336374821', 'Gersik', 'irsyad@gmail.com', 'Perusahaan II'),
-(5, 'Tegar', '082336374821', 'Jember', 'wahyu@gmail.com', 'Perusahaan WI');
+(5, 'Tegar', '082336374821', 'Jember', 'wahyu@gmail.com', 'Perusahaan WI'),
+(6, 'Ibad', '08222', 'Gresik', 'ibad@gmail.com', 'Semen Gresik');
 
 -- --------------------------------------------------------
 
@@ -219,6 +191,70 @@ INSERT INTO `transaksi` (`id_transaksi`, `tanggal_transaksi`, `harga_total`) VAL
 ('TR0003', '2024-05-07', '45000'),
 ('TR0004', '2024-05-08', '50000'),
 ('TR0005', '2024-05-08', '24000');
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in struktur untuk tampilan `view_barang_dan_supplier`
+-- (Lihat di bawah untuk tampilan aktual)
+--
+CREATE TABLE `view_barang_dan_supplier` (
+`nama_supplier` varchar(40)
+,`asal_perusahaan` varchar(225)
+,`nama_barang` varchar(70)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in struktur untuk tampilan `view_barang_supplier_tglkadaluarsa`
+-- (Lihat di bawah untuk tampilan aktual)
+--
+CREATE TABLE `view_barang_supplier_tglkadaluarsa` (
+`nama_supplier` varchar(40)
+,`kode_barang` char(20)
+,`nama_barang` varchar(70)
+,`tgl_kadaluarsa` date
+);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in struktur untuk tampilan `view_transaksi`
+-- (Lihat di bawah untuk tampilan aktual)
+--
+CREATE TABLE `view_transaksi` (
+`id_transaksi` char(10)
+,`tanggal_transaksi` date
+,`harga_total` varchar(25)
+);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur untuk view `view_barang_dan_supplier`
+--
+DROP TABLE IF EXISTS `view_barang_dan_supplier`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_barang_dan_supplier`  AS SELECT `s`.`nama_supplier` AS `nama_supplier`, `s`.`asal_perusahaan` AS `asal_perusahaan`, `sb`.`nama_barang` AS `nama_barang` FROM (`suppliers` `s` join `stok_barang` `sb` on(`s`.`id_supplier` = `sb`.`id_supplier`)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur untuk view `view_barang_supplier_tglkadaluarsa`
+--
+DROP TABLE IF EXISTS `view_barang_supplier_tglkadaluarsa`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_barang_supplier_tglkadaluarsa`  AS SELECT `s`.`nama_supplier` AS `nama_supplier`, `sb`.`kode_barang` AS `kode_barang`, `sb`.`nama_barang` AS `nama_barang`, `db`.`tgl_kadaluarsa` AS `tgl_kadaluarsa` FROM ((`suppliers` `s` join `stok_barang` `sb` on(`s`.`id_supplier` = `sb`.`id_supplier`)) join `detail_barang` `db` on(`sb`.`kode_barang` = `db`.`kode_barang`)) ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur untuk view `view_transaksi`
+--
+DROP TABLE IF EXISTS `view_transaksi`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_transaksi`  AS SELECT `transaksi`.`id_transaksi` AS `id_transaksi`, `transaksi`.`tanggal_transaksi` AS `tanggal_transaksi`, `transaksi`.`harga_total` AS `harga_total` FROM `transaksi` ;
 
 --
 -- Indexes for dumped tables
@@ -253,7 +289,7 @@ ALTER TABLE `kategori`
 -- Indeks untuk tabel `stok_barang`
 --
 ALTER TABLE `stok_barang`
-  ADD PRIMARY KEY (`kode_barang`),
+  ADD PRIMARY KEY (`id_barang`),
   ADD KEY `id_supplier` (`id_supplier`),
   ADD KEY `kategori_barang` (`kategori_barang`);
 
@@ -287,14 +323,14 @@ ALTER TABLE `data_pengguna`
 -- Ketidakleluasaan untuk tabel `detail_barang`
 --
 ALTER TABLE `detail_barang`
-  ADD CONSTRAINT `detail_barang_ibfk_1` FOREIGN KEY (`kode_barang`) REFERENCES `stok_barang` (`kode_barang`);
+  ADD CONSTRAINT `detail_barang_ibfk_1` FOREIGN KEY (`kode_barang`) REFERENCES `stok_barang` (`id_barang`);
 
 --
 -- Ketidakleluasaan untuk tabel `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`kode_barang`) REFERENCES `stok_barang` (`kode_barang`),
-  ADD CONSTRAINT `detail_transaksi_ibfk_3` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`);
+  ADD CONSTRAINT `detail_transaksi_ibfk_3` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`),
+  ADD CONSTRAINT `detail_transaksi_ibfk_4` FOREIGN KEY (`kode_barang`) REFERENCES `stok_barang` (`id_barang`);
 
 --
 -- Ketidakleluasaan untuk tabel `stok_barang`
