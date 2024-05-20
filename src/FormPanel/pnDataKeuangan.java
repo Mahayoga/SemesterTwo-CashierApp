ce4cb1d356849b6a283fd43964ca74fde8027d3a
Mahayoga <myoga.bahtiar@gmail.com>
2024-05-20 07:50:42 +0700

    Nambah fungsi buat tombol cek keuangan

--- src/FormPanel/pnDataKeuangan.java
+++ src/FormPanel/pnDataKeuangan.java
@@ -26,6 +26,7 @@ public class pnDataKeuangan extends javax.swing.JPanel {
     private EditData editData;
     private AmbilData ambilData;
     private HapusData hapusData;
+    private Keuangan keuangan;
     DefaultTableModel model = new DefaultTableModel();
     koneksi db = new koneksi();
 
@@ -52,6 +53,12 @@ public class pnDataKeuangan extends javax.swing.JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 theEventHapus();
+            }
+        });
+        btnKeuangan.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                theEventKeuangan();
             }
         });
         tblData.addMouseListener(new MouseListener() {
@@ -102,6 +109,9 @@ public class pnDataKeuangan extends javax.swing.JPanel {
     public interface HapusData {
         void hapusData();
     }
+    public interface Keuangan {
+        void cekData();
+    }
     
     public void setVariableTambah(TambahData td) {
         this.tambahData = td;
@@ -114,6 +124,9 @@ public class pnDataKeuangan extends javax.swing.JPanel {
     }
     public void setVariableHapus(HapusData hd) {
         this.hapusData = hd;
+    }
+    public void setVariableKeuangan(Keuangan k) {
+        this.keuangan = k;
     }
     
     public void theEventTambah() {
@@ -134,6 +147,11 @@ public class pnDataKeuangan extends javax.swing.JPanel {
     public void theEventHapus() {
         if(hapusData != null) {
             hapusData.hapusData();
+        }
+    }
+    public void theEventKeuangan() {
+        if(keuangan != null) {
+            keuangan.cekData();
         }
     }
     
@@ -247,7 +265,7 @@ public class pnDataKeuangan extends javax.swing.JPanel {
         customButton1 = new CustomComponent.CustomButton();
         cbKategori = new javax.swing.JComboBox<>();
         jLabel1 = new javax.swing.JLabel();
-        customButton6 = new CustomComponent.CustomButton();
+        btnKeuangan = new CustomComponent.CustomButton();
 
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -345,13 +363,13 @@ public class pnDataKeuangan extends javax.swing.JPanel {
         jLabel1.setText("*Pilih berdasarkan Jenis Pengeluaran*");
         add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 90, -1, 30));
 
-        customButton6.setText("customButton6");
-        customButton6.setBackgroundColor(new java.awt.Color(247, 64, 64));
-        customButton6.setFontSize(14);
-        customButton6.setTextBold(1);
-        customButton6.setTextColor(java.awt.Color.white);
-        customButton6.setTheText("Cek Keuangan");
-        add(customButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 490, 140, 40));
+        btnKeuangan.setText("customButton6");
+        btnKeuangan.setBackgroundColor(new java.awt.Color(247, 64, 64));
+        btnKeuangan.setFontSize(14);
+        btnKeuangan.setTextBold(1);
+        btnKeuangan.setTextColor(java.awt.Color.white);
+        btnKeuangan.setTheText("Cek Keuangan");
+        add(btnKeuangan, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 490, 140, 40));
     }// </editor-fold>//GEN-END:initComponents
 
     private void tblDataMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblDataMouseClicked
@@ -400,10 +418,10 @@ public class pnDataKeuangan extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private CustomComponent.CustomButton btnEdit;
     private CustomComponent.CustomButton btnHapus;
+    private CustomComponent.CustomButton btnKeuangan;
     private CustomComponent.CustomButton btnTambah;
     private javax.swing.JComboBox<String> cbKategori;
     private CustomComponent.CustomButton customButton1;
-    private CustomComponent.CustomButton customButton6;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel lbTitle;
